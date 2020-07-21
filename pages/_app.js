99f839e46fa2aad8b5eac24266c3ff935fcf272b
meShakti <svpsctg11@gmail.com>
2020-07-21 23:08:40 +0530

    Implements UI for the list screen

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import 'normalize.css'
+import "../public/css/tailwind.min.css";
+import "../public/css/global.css";
 
 // This default export is required in a new `pages/_app.js` file.
 export default function App({ Component, pageProps }) {
