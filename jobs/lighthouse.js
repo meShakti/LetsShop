e48f6d263906ef28c54e8ca6431f6c297ecd3fab
meShakti <svpsctg11@gmail.com>
2020-08-12 23:21:38 +0530

    #12 : Update Lighthouse link

--- jobs/lighthouse.js
+++ jobs/lighthouse.js
@@ -6,7 +6,7 @@ const { exec } = require("child_process");
 const performLighthouseTests = async () => {
 	const chrome = await chromeLauncher.launch({ chromeFlags: ["--headless"] });
 	const options = { output: "html", port: chrome.port };
-	const testResult = await lighthouse("http://localhost:3000", options);
+	const testResult = await lighthouse("https://lets-shop-4.herokuapp.com", options);
 	const { report } = testResult;
 	fs.writeFileSync("./public/report.html", report);
 	await chrome.kill();
