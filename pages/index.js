a6f2c6430c0aa9e09c733d9f75661035240f1bf8
meena <mcquityppie3@gmail.com>
2020-08-18 20:38:34 +0530

    #1 :trial of push rejection

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Layout from "../components/layout";
 import ProductList from "../components/product/list";
-import productList from "../data/data";
+import productLis from "../data/data";
 
 export default function Index() {
 	return (
 		<Layout>
-			<ProductList products={productList} />
+			<ProductList products={productLis} />
 		</Layout>
 	);
 }
