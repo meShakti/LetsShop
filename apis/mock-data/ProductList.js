3c1a938b0c1f1e634a07d5f06caa2e389c86d073
meShakti <svpsctg11@gmail.com>
2020-08-31 16:53:10 +0530

    #24 : Implements #24

---
+++ apis/mock-data/ProductList.js
@@ -0,0 +1,95 @@
+
+const productList = [
+	{
+		id: "random_id_product",
+		name: "Pregnant Woman Slide Sleepers",
+		description: "Bedding Pillow",
+		cost: 770,
+		currency: "INR",
+		image:
+      "https://static.wixstatic.com/media/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.webp",
+	},
+	{
+		id: "random_id_product",
+		name: "Maya Calendar Pillow",
+		description: "Bedding Pillow",
+		cost: 100,
+		currency: "INR",
+		image:
+      "https://static.wixstatic.com/media/fddfe9b3f65549db82d0567bdbe141f3.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/fddfe9b3f65549db82d0567bdbe141f3.webp",
+	},
+	{
+		id: "random_id_product",
+		name: "Pregnant Woman Slide Sleepers",
+		description: "Bedding Pillow",
+		cost: 770,
+		currency: "INR",
+		image:
+      "https://static.wixstatic.com/media/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.webp",
+	},
+	{
+		id: "random_id_product",
+		name: "Maya Calendar Pillow",
+		description: "Bedding Pillow",
+		cost: 100,
+		currency: "INR",
+		image:
+      "https://static.wixstatic.com/media/fddfe9b3f65549db82d0567bdbe141f3.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/fddfe9b3f65549db82d0567bdbe141f3.webp",
+	},
+	{
+		id: "random_id_product",
+		name: "Pregnant Woman Slide Sleepers",
+		description: "Bedding Pillow",
+		cost: 770,
+		currency: "INR",
+		image:
+      "https://static.wixstatic.com/media/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.webp",
+	},
+	{
+		id: "random_id_product",
+		name: "Maya Calendar Pillow",
+		description: "Bedding Pillow",
+		cost: 100,
+		currency: "INR",
+		image:
+      "https://static.wixstatic.com/media/fddfe9b3f65549db82d0567bdbe141f3.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/fddfe9b3f65549db82d0567bdbe141f3.webp",
+	},
+	{
+		id: "random_id_product",
+		name: "Pregnant Woman Slide Sleepers",
+		description: "Bedding Pillow",
+		cost: 770,
+		currency: "INR",
+		image:
+      "https://static.wixstatic.com/media/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.webp",
+	},
+	{
+		id: "random_id_product",
+		name: "Maya Calendar Pillow",
+		description: "Bedding Pillow",
+		cost: 100,
+		currency: "INR",
+		image:
+      "https://static.wixstatic.com/media/fddfe9b3f65549db82d0567bdbe141f3.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/fddfe9b3f65549db82d0567bdbe141f3.webp",
+	},
+	{
+		id: "random_id_product",
+		name: "Pregnant Woman Slide Sleepers",
+		description: "Bedding Pillow",
+		cost: 770,
+		currency: "INR",
+		image:
+      "https://static.wixstatic.com/media/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.webp",
+	},
+	{
+		id: "random_id_product",
+		name: "Maya Calendar Pillow",
+		description: "Bedding Pillow",
+		cost: 100,
+		currency: "INR",
+		image:
+      "https://static.wixstatic.com/media/fddfe9b3f65549db82d0567bdbe141f3.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/fddfe9b3f65549db82d0567bdbe141f3.webp",
+	},
+];
+
+export default productList;
