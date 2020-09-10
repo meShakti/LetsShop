f09af9f859e4517cc1b2a30ee1b6fe1eb54e4d9d
meShakti <svpsctg11@gmail.com>
2020-09-10 23:17:32 +0530

    #26 : Implements Header as per current requirement

--- src/components/layout/header/index.js
+++ src/components/layout/header/index.js
@@ -23,7 +23,7 @@ const Header = ({counter}) => {
 						<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" width="40px" height="40px"><path d="M0 0h24v24H0z" fill="none" /><path d="M7 18c-1.1 0-1.99.9-1.99 2S5.9 22 7 22s2-.9 2-2-.9-2-2-2zM1 2v2h2l3.6 7.59-1.35 2.45c-.16.28-.25.61-.25.96 0 1.1.9 2 2 2h12v-2H7.42c-.14 0-.25-.11-.25-.25l.03-.12.9-1.63h7.45c.75 0 1.41-.41 1.75-1.03l3.58-6.49c.08-.14.12-.31.12-.48 0-.55-.45-1-1-1H5.21l-.94-2H1zm16 16c-1.1 0-1.99.9-1.99 2s.89 2 1.99 2 2-.9 2-2-.9-2-2-2z" /></svg>
 					</a>
 				</div>
-				<span className="font-semibold text-xl tracking-tight">New Abhishek</span>
+				<a href="/"><span className="font-semibold text-xl tracking-tight">New Abhishek</span></a>
 			</div>
 
 			<div className="block lg:hidden">
@@ -33,9 +33,9 @@ const Header = ({counter}) => {
 			</div>
 			<div className={`${isExpanded ? "block" : "hidden"} w-full block flex-grow lg:flex lg:items-center lg:w-auto`}>
 				<div className="lg:flex lg:items-stretch lg:justify-end ml-auto">
-					<a href="#" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center  hover:bg-grey-dark">Item 1</a>
-					<a href="#" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center  hover:bg-grey-dark">Item 2</a>
-					<a href="#" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center  hover:bg-grey-dark">Item 3</a>
+					<a href="#" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center  hover:bg-grey-dark">My Addresses</a>
+					<a href="#" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center  hover:bg-grey-dark">My orders</a>
+					<a href="#" className="flex-no-grow flex-no-shrink relative py-2 px-4 leading-normal text-white no-underline flex items-center  hover:bg-grey-dark">Guest</a>
 				</div>
 
 
