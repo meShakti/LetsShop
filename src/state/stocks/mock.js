e185dd20ba1804de6aba192c524ed5e1c7aafcb2
meShakti <svpsctg11@gmail.com>
2020-09-17 19:14:39 +0530

    #23 : Add mock data

--- src/state/stocks/mock.js
+++ src/state/stocks/mock.js
@@ -1,95 +1,168 @@
 
 const productList = [
 	{
-		id: "random_id_product",
-		name: "Pregnant Woman Slide Sleepers",
-		description: "Bedding Pillow",
-		cost: 770,
+		id: "12354",
+		name: "Limraz furniture L 56 Engineered Wood Study Table",
+		description:"Free Standing, Finish Color - brown",
+		rating:4.2,
+		totalRatings:1101,
+		cost: 1104,
+		mrp:1200,
 		currency: "₹",
+		availability:{
+			status:"available"
+		},
+		benefits:[
+			"No Cost EMI"
+		],
 		image:
-      "https://static.wixstatic.com/media/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.webp",
+	"https://rukminim1.flixcart.com/image/352/352/k6l2vm80/computer-table/z/y/a/mdf-fk-28-limraz-furniture-brown-original-imafzy9ytkbegxg4.jpeg?q=70",
 	},
 	{
-		id: "random_id_product",
-		name: "Maya Calendar Pillow",
-		description: "Bedding Pillow",
-		cost: 100,
+		id: "12353",
+		name: "OKRA Vento Engineered Wood Study Table ",
+		description:"Free Standing, Finish Color - African Walnut",
+		rating:0,
+		totalRatings:0,
+		cost: 3695,
+		mrp:4990,
 		currency: "₹",
+		availability:{
+			status:"available"
+		},
+		benefits:[
+			"No Cost EMI"
+		],
 		image:
-      "https://static.wixstatic.com/media/fddfe9b3f65549db82d0567bdbe141f3.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/fddfe9b3f65549db82d0567bdbe141f3.webp",
+	"https://rukminim1.flixcart.com/image/416/416/ke8uv0w0/office-study-table/h/h/5/particle-board-ven2aw-okra-african-walnut-original-imafuyutgfbf2w7v.jpeg?q=70",
 	},
 	{
-		id: "random_id_product",
-		name: "Pregnant Woman Slide Sleepers",
-		description: "Bedding Pillow",
-		cost: 770,
+		id: "12352",
+		name: "limraz furniture Engineered Wood 4 Seater Dining Table",
+		description:"Free Standing, Finish Color - brown",
+		rating:4.2,
+		totalRatings:1101,
+		cost: 1104,
+		mrp:1200,
+		availability:{
+			status:"available"
+		},
 		currency: "₹",
+		benefits:[
+			"No Cost EMI"
+		],
 		image:
-      "https://static.wixstatic.com/media/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.webp",
+	"https://rukminim1.flixcart.com/image/416/416/jt395zk0/dining-table/z/j/t/4-seater-carbon-steel-lm01-limraz-furniture-brown-original-imafegnbmprkpypa.jpeg?q=70",
 	},
 	{
-		id: "random_id_product",
-		name: "Maya Calendar Pillow",
-		description: "Bedding Pillow",
-		cost: 100,
+		id: "12351",
+		name: "Sleepyhead Flip - Dual Sided 5 inch Single High Density (HD) Foam Mattress",
+		description:"Vacuum Packed",
+		rating:4.2,
+		totalRatings:5224,
+		cost: 5224,
+		mrp:7499,
 		currency: "₹",
+		availability:{
+			status:"available"
+		},
+		benefits:[
+			"No Cost EMI"
+		],
 		image:
-      "https://static.wixstatic.com/media/fddfe9b3f65549db82d0567bdbe141f3.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/fddfe9b3f65549db82d0567bdbe141f3.webp",
+	"https://rukminim1.flixcart.com/image/416/416/k6xxua80/bed-mattress/8/z/v/single-5-72-36-flip-dual-sided-high-density-foam-mattress-with-original-imafp9ws7negpsbz.jpeg?q=70",
 	},
 	{
-		id: "random_id_product",
-		name: "Pregnant Woman Slide Sleepers",
-		description: "Bedding Pillow",
-		cost: 770,
+		id: "12350",
+		name: "Spacewood Mayflower Engineered Wood Queen Box Bed",
+		description:"Finish Color - Brown",
+		rating:3.9,
+		totalRatings:6176,
+		cost: 15690,
+		mrp:31140,
 		currency: "₹",
+		availability:{
+			status:"available"
+		},
+		benefits:[
+			"No Cost EMI"
+		],
 		image:
-      "https://static.wixstatic.com/media/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.webp",
+	"https://rukminim1.flixcart.com/image/416/416/jk8lz0w0/bed/g/e/z/queen-na-particle-board-kosmo-mayflower-queen-bed-box-spacewood-original-imaf7mtfdv8hfg6z.jpeg?q=70",
 	},
 	{
-		id: "random_id_product",
-		name: "Maya Calendar Pillow",
-		description: "Bedding Pillow",
-		cost: 100,
+		id: "12349",
+		name: "Perfect Homes Zouk TV Entertainment Unit",
+		description:"Finish Color - Wenge",
+		rating:4.3,
+		totalRatings:1101,
+		cost: 3990,
+		mrp:5799,
+		availability:{
+			status:"available"
+		},
 		currency: "₹",
+		benefits:[
+			"No Cost EMI"
+		],
 		image:
-      "https://static.wixstatic.com/media/fddfe9b3f65549db82d0567bdbe141f3.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/fddfe9b3f65549db82d0567bdbe141f3.webp",
+	"https://rukminim1.flixcart.com/image/416/416/k6l2vm80/tv-entertainment-unit/z/q/2/particle-board-fk7002-flipkart-perfect-homes-dark-wenge-original-imafzpdchnhnnrtg.jpeg?q=70",
 	},
 	{
-		id: "random_id_product",
-		name: "Pregnant Woman Slide Sleepers",
-		description: "Bedding Pillow",
-		cost: 770,
+		id: "12348",
+		name: "Perfect Homes Leatherette Office Arm Chair ",
+		description:"Black",
+		rating:4.2,
+		totalRatings:1027,
+		cost: 9290,
+		mrp:13004,
+		availability:{
+			status:"available"
+		},
 		currency: "₹",
+		benefits:[
+			"No Cost EMI",
+		],
 		image:
-      "https://static.wixstatic.com/media/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.webp",
+	"https://rukminim1.flixcart.com/image/352/352/jehf4i80/office-study-chair/h/g/z/pu-leatherette-scbla001-perfect-homes-by-flipkart-original-imaf2bwghxqjs9w9.jpeg?q=70",
 	},
 	{
-		id: "random_id_product",
-		name: "Maya Calendar Pillow",
-		description: "Bedding Pillow",
-		cost: 100,
+		id: "12347",
+		name: "MemeHO BL Wood Portable Laptop Table",
+		description:"Finish Color - Black",
+		rating:4.4,
+		totalRatings:14101,
+		cost: 899,
+		mrp:1499,
 		currency: "₹",
+		availability:{
+			status:"scarce",
+			stockLeft:10
+		},
+		benefits:[
+			"No Cost EMI"
+		],
 		image:
-      "https://static.wixstatic.com/media/fddfe9b3f65549db82d0567bdbe141f3.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/fddfe9b3f65549db82d0567bdbe141f3.webp",
+	"https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTDcYNa7kiMBM6rRU_vaaecpVkmSVYWmzUrgWvIVv_yWxFfefuiVokj954gUEePez2s65NHmsU&usqp=CAc",
 	},
 	{
-		id: "random_id_product",
-		name: "Pregnant Woman Slide Sleepers",
-		description: "Bedding Pillow",
-		cost: 770,
+		id: "12346",
+		name: "limraz furniture Engineered Wood 4 Seater Dining Table",
+		description:"Finish Color - brown",
+		rating:4.4,
+		totalRatings:93,
+		cost: 1520,
+		mrp:3800,
 		currency: "₹",
+		availability:{
+			status:"unavailable"
+		},
+		benefits:[
+			"No Cost EMI"
+		],
 		image:
-      "https://static.wixstatic.com/media/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/ca9432_2ae9d7110e694f769a865a424aef4e84~mv2.webp",
-	},
-	{
-		id: "random_id_product",
-		name: "Maya Calendar Pillow",
-		description: "Bedding Pillow",
-		cost: 100,
-		currency: "₹",
-		image:
-      "https://static.wixstatic.com/media/fddfe9b3f65549db82d0567bdbe141f3.jpg/v1/fill/w_560,h_560,al_c,q_80,usm_0.66_1.00_0.01/fddfe9b3f65549db82d0567bdbe141f3.webp",
-	},
+	"https://rukminim1.flixcart.com/image/352/352/k6s82vk0/dining-table/x/k/x/2-seater-mdf-fk-27-limraz-furniture-dark-brown-original-imafp5b7asavkn55.jpeg?q=70",
+	}
 ];
 
 export default productList;
