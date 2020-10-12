579281e3dd8700f181c6615aa54312d487050f1e
meShakti <svpsctg11@yahoo.com>
2020-10-12 23:06:02 +0530

    #34 : Functional implementation of 34

--- src/state/stocks/reducer.js
+++ src/state/stocks/reducer.js
@@ -8,28 +8,30 @@ const initialState = {
 export default function(state = initialState, action) {
 	let isItemAlreadyInCart = false;
 	let countReachesZero = false;
-	let modifiedCart = state.cart.map((item)=>{
-		if(item.id===action.payload.id){
-			item.quantity-=1;
-			countReachesZero = item.quantity===0;
-		}
-		return item;
-	});
-	const newcart = state.cart.map((item)=>{
-		if(item.id===action.payload.id){
-			item.quantity+=1;
-			isItemAlreadyInCart = true;
-		}
-		return item;
-	});
+	let modifiedCart;
+	let newcart;
 	switch (action.type) {
 	case ADD_TO_CART:
+		newcart =  state.cart.map((item)=>{
+			if(item.id===action.payload.id){
+				item.quantity+=1;
+				isItemAlreadyInCart = true;
+			}
+			return item;
+		});
 		!isItemAlreadyInCart && newcart.push(action.payload);
 		return { ...state,cart:newcart,
 			saveForLater: state.saveForLater
 				.filter((stock)=>stock.id!==action.payload.id) };
 	
 	case SUBTRACT_ITEM_FROM_CART:
+		modifiedCart = state.cart.map((item)=>{
+			if(item.id===action.payload.id){
+				item.quantity-=1;
+				countReachesZero = item.quantity===0;
+			}
+			return item;
+		});
 		if(countReachesZero){
 			return { ...state, cart: state.cart
 				.filter((stock)=>stock.id!==action.payload.id)};
